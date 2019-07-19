7350de288d531183db71ea7e0719480127d59d41
Asoul <azx754@gmail.com>
2019-07-19 15:23:33 +0800

    #1, Make post id public

--- contracts/OriginalPost.sol
+++ contracts/OriginalPost.sol
@@ -2,7 +2,7 @@ pragma solidity >0.4.99 <0.6.0;
 
 contract OriginalPost {
   event Posted(uint256 postIdx, string data);
-  uint256 postIdx;
+  uint256 public postIdx;
 
   function Post(string memory data) public {
     emit Posted(postIdx, data);
