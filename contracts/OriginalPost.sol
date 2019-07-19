8e50341279ae8b6914a95d1a56087538c8e0c4d0
Asoul <azx754@gmail.com>
2019-07-19 19:28:11 +0800

    #1, Add creator in event

--- contracts/OriginalPost.sol
+++ contracts/OriginalPost.sol
@@ -1,11 +1,11 @@
 pragma solidity >0.4.99 <0.6.0;
 
 contract OriginalPost {
-  event Posted(uint256 indexed postIdx, string data);
+  event Posted(uint256 indexed postIdx, address indexed creator, string data);
   uint256 public postIdx;
 
   function Post(string memory data) public {
-    emit Posted(postIdx, data);
+    emit Posted(postIdx, msg.sender, data);
     postIdx += 1;
   }
 }
