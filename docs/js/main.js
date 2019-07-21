229aa47d1a5ddbcd536204f28cc8cfbadb01ad58
Asoul <azx754@gmail.com>
2019-07-21 18:43:28 +0800

    Add basic create post function

--- docs/js/main.js
+++ docs/js/main.js
@@ -94,13 +94,38 @@ async function closePostDialog () {
 }
 
 async function createPost () {
-
+  let textArea = document.querySelector('#post-content')
+  let accounts = await window.ethereum.enable()
+  if (window.hasMetamask && accounts.length > 0) {
+    await window.Post.methods.Create(textArea.value).send({
+      from: accounts[0]
+    })
+    // Clean text area after finish
+    textArea.value = ''
+  } else {
+    alert('Please install metamask plugin')
+  }
 }
 
 async function startApp () {
   const rpcUrl = 'https://mainnet.infura.io/v3/86955966e8f84fe2be3f95293a27aefe'
 
-  window.web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl))
+  // Check network status
+  window.hasMetamask = false
+  if (typeof web3 !== 'undefined') {
+    window.oldWeb3 = web3
+    window.web3 = new Web3(web3.currentProvider)
+    let networkType = await window.web3.eth.net.getNetworkType()
+    if (networkType === 'main') {
+      window.hasMetamask = true
+      window.web3 = new Web3(web3.currentProvider)
+    }
+  }
+  if (!window.hasMetamask) {
+    window.web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl))
+  }
+
+  // Initialize web3 modules
   window.Post = new window.web3.eth.Contract(JSON.parse(`[{"constant":false,"inputs":[{"name":"data","type":"string"}],"name":"Create","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"postIdx","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"postIdx","type":"uint256"},{"indexed":true,"name":"creator","type":"address"},{"indexed":false,"name":"data","type":"string"}],"name":"Posted","type":"event"}]`), '0xEbfc4A31F0C1a8002398AE5601bE27c6a7ed35B7')
   window.Username = new window.web3.eth.Contract(JSON.parse(`[{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"username","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_username","type":"bytes32"}],"name":"Update","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"used","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"user","type":"address"},{"indexed":false,"name":"username","type":"bytes32"}],"name":"Updated","type":"event"}]`), '0xf35974226f5A7464D1B39AF1c11a3e2109e7C694')
 
@@ -121,6 +146,7 @@ async function startApp () {
   document.querySelector('#post-dialog .popup').addEventListener('click', function (event) {
     event.stopPropagation()
   })
+  document.querySelector('#create-post').addEventListener('click', createPost)
 }
 
 window.addEventListener('load', function () {
