49dd4da176dee810145267185bd1ffd4d4ee1f7a
Asoul <azx754@gmail.com>
2019-07-21 18:41:32 +0800

    Add reverse search for username

--- contracts/Username.sol
+++ contracts/Username.sol
@@ -1,16 +1,16 @@
 pragma solidity >0.4.99 <0.6.0;
 
 contract Username {
-  event Updated(address indexed user, bytes32 username);
+  event Updated(address indexed user, bytes32 indexed username);
 
   mapping(address => bytes32) public username;
-  mapping(bytes32 => bool) public used;
+  mapping(bytes32 => address) public owner;
 
   function Update(bytes32 _username) public {
-    require(!used[_username]);
+    require(owner[_username] == address(0));
     bytes32 oldUserName = username[msg.sender];
-    used[_username] = true;
-    used[oldUserName] = false;
+    owner[_username] = msg.sender;
+    owner[oldUserName] = address(0);
     username[msg.sender] = _username;
     emit Updated(msg.sender, _username);
   }
