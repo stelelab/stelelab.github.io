e457e86da0fecd893f9e90e530cd513456811c45
Asoul <azx754@gmail.com>
2019-07-22 00:06:29 +0800

    Add user information

--- docs/js/user.js
+++ docs/js/user.js
@@ -1,15 +1,35 @@
 window.addEventListener('coreLoaded', async function () {
-  let username = window.location.hash.replace('#', '')
-  let address
-  if (username.startsWith('0x')) {
-    address = username
+  let hash = window.location.hash.replace('#', '')
+  let address, username
+  if (hash.startsWith('0x')) {
+    address = hash
+    username = await window._xpost.getUsername(hash)
   } else {
-    address = await window._xpost.getAddress(username)
+    address = await window._xpost.getAddress(hash)
     if (address === '0x0000000000000000000000000000000000000000') {
-      document.querySelector('#user-not-found').textContent = `User "${username}" not exists`
+      document.querySelector('#user-not-found').textContent = `User "${hash}" not exists`
       document.querySelector('#user-not-found').style.display = 'block'
       return
     }
+    username = hash
   }
+
+  if (username.length > 0) {
+    document.querySelector('#user-information .username').textContent = `@${username}`
+  }
+  document.querySelector('#user-information .address').textContent = address
+
+  // Load description
+  let descriptionIdx = await window._xpost.Description.methods.description(address).call()
+  let description = await window._xpost.Description.getPastEvents('Updated', {
+    fromBlock: 8194784,
+    filter: {
+      descriptionIdx: [descriptionIdx]
+    }
+  })
+  if (description.length > 0) {
+    document.querySelector('#user-information .description').textContent = description[0].returnValues.data
+  }
+
   window._xpost.loadPageWithUserAddress(address)
 })
